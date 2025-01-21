cd8ca96cc884f3c7eed7a52e99be0160917afd44
Rinsaf14 <ririnsaf14@gmail.com>
2025-01-21 07:58:54 +0700

    First Commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>© 2023 FMIPA ICP Student Community (FISCo) UNM</p>
+      <ul>
+        <li>Terms of Services</li>
+        <li>Privacy Policy</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
